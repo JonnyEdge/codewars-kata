ded6960d15a1994442a744882f86d01f21042549
Jonny Edge <jonnyedge@outlook.com>
2019-02-18 21:12:57 +0000

    Number to String kata completed.

---
+++ number-to-string.js
@@ -0,0 +1,13 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+function numberToString(num) {
+  const string = num.toString()
+  return string
+}
+
+console.log(numberToString(1))
+console.log(numberToString(12))
+console.log(numberToString(123))
+console.log(numberToString(1234))
